ee4e9e2b0116947dec6f5220442e9583e52a79fa
guyunting99 <guyunting@gooday012.com>
2018-08-13 14:01:43 +0800

    r

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ var adct = document.getElementsByTagName('title')[0].getAttribute('adct');
 
 document.writeln("<footer>\n" +
     "    <div class=\"footerTop1\">\n" +
-    "        <div class=\"mianCont shouye\" style=\"width: 1230px;height: 90px;background: url(../img/footers_hzf.png) 0 0 no-repeat\">\n" +
+    "        <div class=\"mianCont shouye\" style=\"width: 1230px;height: 90px;background: url(../img/footers_hzf1.png) 0 0 no-repeat\">\n" +
     // "            <div class=\"Lf ftphone\">\n" +
     // "                <div class=\"Lf ftphoneImg ftImg1\">\n" +
     // "                    <!--<img src=\"img/footer1.png\" alt=\"\">-->\n" +
