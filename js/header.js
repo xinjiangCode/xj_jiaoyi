72acd5dc6c78a7b90b67ffd1996bf17af2cd55af
huzhengfeng <huzhengfeng@gooday123.com>
2018-08-15 21:52:25 +0800

    22

--- js/header.js
+++ js/header.js
@@ -556,10 +556,10 @@ if(adct == '首页' ){
 
 document.writeln(headLink);
 //var headerip = 'http://api.xjv56.com/service/';
- // var headerip = 'http://192.168.1.80:7777/service/';
+var headerip = 'http://192.168.1.80:7777/service/';
 //var headerip = 'http://127.0.0.1:7777/service/';
 
-var headerip = 'http://192.168.1.219:7777/service/';
+//var headerip = 'http://192.168.1.219:7777/service/';
 //header ip
 window.onload = function(){
 	   //全部分类
