37f405e3733de6c20b678c6a04fc00392481f670
guyunting99 <guyunting@gooday012.com>
2018-09-13 16:00:19 +0800

    e

--- js/header.js
+++ js/header.js
@@ -579,7 +579,7 @@ document.writeln(headLink);
 // var headerip = 'http://api.xjv56.com/service/';
 // var headerip = 'http://api.test.xjv56.com/service/';
 var headerip = 'http://192.168.1.80:7777/service/';
-// var headerip = 'http://192.168.1.17:7777/service/';
+// var headerip = 'http://192.168.1.202:7777/service/';
 
 // var headerip = 'http://127.0.0.1:7777/service/';
 
