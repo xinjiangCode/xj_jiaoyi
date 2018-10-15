ce72935c047fb12a5572eb17d585af7f087f95cc
guyunting99 <guyunting@gooday012.com>
2018-10-15 21:40:25 +0800

    c

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 //api地址
 // var pubIP = 'http://api.xjv56.com/service/';
-// var pubIP = 'http://api.test.xjv56.com/service/';
-var pubIP = 'http://192.168.1.80:7777/service/';
+var pubIP = 'http://api.test.xjv56.com/service/';
+// var pubIP = 'http://192.168.1.80:7777/service/';
 // var pubIP = 'http://192.168.1.72:7777/service/';
 
 //导出
